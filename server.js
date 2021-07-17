7d915a9eb900a4e3cad44e05b7ba1fbc09c01ad4
Virginia Rabello <virginia.rabello2107@gmail.com>
2021-07-17 18:00:33 -0500

    User controllers and routes tested in the insomnia

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/creamy-bug', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
